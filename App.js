b53de4442f782981ba34948e37ca9cc6b9709307
Sophie Mann <sklm@live.co.uk>
2020-06-02 13:35:45 +0100

    Add store

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import Main from './components/Main';
+import store from './redux/store/index';
+import { Provider } from 'react-redux';
 
 class App extends Component {
   render() {
-    return <Main />;
+    return (
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    );
   }
 }
 
