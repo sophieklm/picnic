d77cbe403af56187953978d72020a0e5f0de4e3a
Sophie Mann <sklm@live.co.uk>
2020-06-02 15:41:24 +0100

    Add dispatch delete to list

--- components/List.js
+++ components/List.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteIngredient } from '../redux/actions';
 
 const List = (props) => {
+  const ingredients = useSelector((state) => state.ingredients);
+  const dispatch = useDispatch();
   return (
     <View>
-      {props.ingredients.map((el, i) => {
+      {ingredients.map((el, i) => {
         return (
           <View style={styles.space} key={i}>
             <View style={styles.row}>
               <View style={styles.dot} />
               <Text>{el}</Text>
             </View>
+            <Text
+              onPress={() => {
+                dispatch(deleteIngredient(i));
+              }}>
+              &#x1F5D1;
+            </Text>
           </View>
         );
       })}
